d602f91830530e8d840529341b86fb779b8c8b91
shreya81601 <71819463+shreya81601@users.noreply.github.com>
2020-11-12 18:44:04 +0530

    final

--- own_script.js
+++ own_script.js
@@ -1,13 +1,12 @@
 var sounds = [
-  "audio/hands.mp3",
+  "audio/Hands2.wav",
   "audio/Rice.wav",
-  "audio/lightning.mp3",
-  "audio/rain_dist2.mp3",
-  "audio/whistle.mp3",
-  "audio/thunder.mp3",
-  "audio/murmure.mp3",
-  "audio/creek.mp3"
-
+  "audio/lightning.wav",
+  "audio/rain_dist2.wav",
+  "audio/whistle.wav",
+  "audio/fook.wav",
+  "audio/murmure.wav",
+  "audio/pouring.wav"
 ];
 
 
@@ -23,28 +22,36 @@ for (var i = 0; i < sounds.length; i++) {
   // create a button to trigger the sounds
   const btn = document.createElement("button");
   if (i==0){
-    btn.innerHTML = "Medium Rain";
+    btn.innerHTML = "Wind Noise";
+    btn.style.fontFamily= "Overlock"; 
   } 
   else if(i==1){
     btn.innerHTML = "Heavy Rain Drops";
+    btn.style.fontFamily= "Overlock"; 
   }
   else if(i==2){
-    btn.innerHTML = "Lightning";
+    btn.innerHTML = "Thunder";
+    btn.style.fontFamily= "Overlock"; 
   }
   else if(i==3){
     btn.innerHTML = "Rain in Distance";
+    btn.style.fontFamily= "Overlock"; 
   }
   else if(i==4){
     btn.innerHTML = "Light Wind Noise";
+    btn.style.fontFamily= "Overlock"; 
   }
   else if(i==5){
-    btn.innerHTML = "Thunder";
+    btn.innerHTML = "Strong Wind";
+    btn.style.fontFamily= "Overlock"; 
   }
   else if(i==6){
     btn.innerHTML = "Rain Murmur";
+    btn.style.fontFamily= "Overlock"; 
   }
   else if(i==7){
     btn.innerHTML = "Rain creek";
+    btn.style.fontFamily= "Overlock"; 
   }
  
 
@@ -55,9 +62,71 @@ for (var i = 0; i < sounds.length; i++) {
       Rain_player.play();
       Rain_player.loop=true;
       btn.className = "active";
+            if (btn.innerHTML=="Wind Noise")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Rub hands";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Heavy Rain Drops")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Drop rice on metal";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Thunder")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Real thunder!";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Rain in Distance")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Computer fan";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Light Wind Noise")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Whistle";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Strong Wind")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Blow into mic";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Rain Murmur")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Shake rice";
+                }, 4000);
+              }
+            else if (btn.innerHTML=="Rain creek")
+              { 
+                timeout = setTimeout(function() {
+                btn.innerHTML = "Pour water";
+                }, 4000);
+              }
     } else {
       Rain_player.pause();
       btn.className = "";
     }
   });
+
+  // btn.addEventListener("click", function(){
+  //   if (btn.value=="Medium Rain") elem.value = "Hands rubbing";
+    
+  // }
+
+
+
+
+
+
+
+
+
 }
